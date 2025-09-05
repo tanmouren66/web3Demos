de7949aa1f704129e0d7e80aefd9c575702fdbc6
tanmouren <tanmouren@163.com>
2025-09-05 17:50:56 +0800

    '暂存提交查看'

--- helloword/hello.sol
+++ helloword/hello.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT 
 
-pragma solidity >=0.6.12 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 contract Hello {
     function sayHello() public pure returns(string memory){
         return "hello world";
+    }
+    uint[] testArray= [uint (1),2,3,4];
+
+    function deleteFromArray (uint _index) public{
+        _index = _index-1;
+        require(_index <testArray.length,"index is too big");
+        for (uint256 index = _index; index < testArray.length; index++) {
+            testArray[index] = testArray[index+1];
+            
+        }
+        testArray.pop;
+    }
+    function printArray()view public returns ( uint[] memory) {
+       return testArray;
     }
 }
