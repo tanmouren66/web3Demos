77f2ef426e4966036eb13cdbb9740620980c6009
tanmouren66 <tanxilincn@163.com>
2025-09-09 09:45:52 +0800

    从数组删除值，打印日志

--- helloword/hello.sol
+++ helloword/hello.sol
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: MIT 
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 pragma solidity >=0.8.0 <0.9.0;
 contract Hello {
+    using Strings for uint256;
+
     function sayHello() public pure returns(string memory){
         return "hello world";
     }
+    event LogInfo(address sender,string value);
     uint[] testArray= [uint (1),2,3,4];
 
     function deleteFromArray (uint _index) public{
-        _index = _index-1;
         require(_index <testArray.length,"index is too big");
-        for (uint256 index = _index; index < testArray.length; index++) {
-            testArray[index] = testArray[index+1];
-            
+
+        for (uint256 i = _index; i < testArray.length-1; i++) {
+            testArray[i] = testArray[i+1];
         }
-        testArray.pop;
+        testArray.pop();
+        string memory log =   string(abi.encodePacked("delete from array #", _index.toString()));
+        emit LogInfo(msg.sender,log);
+
     }
     function printArray()view public returns ( uint[] memory) {
        return testArray;
